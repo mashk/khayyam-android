f24d1bfeb78057111f82a65d028a37fe1a90cb25
sajad <sajad@shokri.net>
2015-06-29 01:47:09 +0430

    resolve #4

---
+++ app/src/main/java/ir/coderz/khayyam_android/model/entities/poem/Poem.java
@@ -0,0 +1,43 @@
+package ir.coderz.khayyam_android.model.entities.poem;
+
+/**
+ * Created by sajad on 6/29/15.
+ */
+public class Poem {
+    private String one;
+    private String two;
+    private String three;
+    private String four;
+
+    public String getOne() {
+        return one;
+    }
+
+    public void setOne(String one) {
+        this.one = one;
+    }
+
+    public String getTwo() {
+        return two;
+    }
+
+    public void setTwo(String two) {
+        this.two = two;
+    }
+
+    public String getThree() {
+        return three;
+    }
+
+    public void setThree(String three) {
+        this.three = three;
+    }
+
+    public String getFour() {
+        return four;
+    }
+
+    public void setFour(String four) {
+        this.four = four;
+    }
+}
