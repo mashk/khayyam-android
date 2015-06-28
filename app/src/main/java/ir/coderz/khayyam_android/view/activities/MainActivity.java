a81b4ed07e919c477a1a1577297d16d5a03ff12a
sajad <sajad@shokri.net>
2015-06-28 19:18:07 +0430

    add main activity with simple drawer

---
+++ app/src/main/java/ir/coderz/khayyam_android/view/activities/MainActivity.java
@@ -0,0 +1,24 @@
+package ir.coderz.khayyam_android.view.activities;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import ir.coderz.khayyam_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
