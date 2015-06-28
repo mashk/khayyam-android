1097cb0aa607a1237fc83c58b234edb5ef7711ef
sajad <sajad@shokri.net>
2015-06-28 19:28:27 +0430

    toggle navigation

--- app/src/main/java/ir/coderz/khayyam_android/view/activities/MainActivity.java
+++ app/src/main/java/ir/coderz/khayyam_android/view/activities/MainActivity.java
@@ -1,24 +1,55 @@
 package ir.coderz.khayyam_android.view.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import ir.coderz.khayyam_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    @Bind(R.id.drawer)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.navigation)
+    NavigationView navigation;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
 
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        drawerToggle = new ActionBarDrawerToggle
+                (
+                        this,
+                        drawerLayout,
+                        toolbar,
+                        R.string.drawer_open,
+                        R.string.drawer_close
+                );
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 }
