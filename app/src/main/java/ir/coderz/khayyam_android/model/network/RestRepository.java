7f41b59869d581caf6ca36ee70bbdf776f8daee1
sajad <sajad@shokri.net>
2015-07-01 00:40:12 +0430

    implement rest client. resolves #5

---
+++ app/src/main/java/ir/coderz/khayyam_android/model/network/RestRepository.java
@@ -0,0 +1,33 @@
+package ir.coderz.khayyam_android.model.network;
+
+import java.util.List;
+
+import ir.coderz.khayyam_android.model.Repository;
+import ir.coderz.khayyam_android.model.entities.information.Info;
+import ir.coderz.khayyam_android.model.entities.poem.Poem;
+import retrofit.RestAdapter;
+import rx.Observable;
+
+/**
+ * Created by sajad on 6/30/15.
+ */
+public class RestRepository implements Repository {
+    RestAPI restAPI;
+
+    public RestRepository() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(RestAPI.END_POINT)
+                .build();
+        restAPI = restAdapter.create(RestAPI.class);
+    }
+
+    @Override
+    public Observable<Info> getInfo() {
+        return restAPI.getInfo();
+    }
+
+    @Override
+    public Observable<List<Poem>> getPoems(String editor) {
+        return restAPI.getPoems(editor);
+    }
+}
